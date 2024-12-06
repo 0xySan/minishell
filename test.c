4cb6047afd2b275ba6328295057f3c06fdad90f8
0xySan <=>
2024-12-06 14:48:53 +0100

    finished cd

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: etaquet <etaquet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 16:18:53 by etaquet           #+#    #+#             */
-/*   Updated: 2024/12/06 14:32:49 by etaquet          ###   ########.fr       */
+/*   Updated: 2024/12/06 14:47:45 by etaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,15 +66,45 @@ int	check_if_accessible(char *cwd, char *path)
 	return (accessible);
 }
 
+int	check_if_raccessible(char *cwd, char *path)
+{
+	char	*nw_path;
+	int		accessible;
+	char	*home;
+	char	*tmp;
+
+	home = getenv("HOME");
+	nw_path = malloc(ft_strlen(home) + ft_strlen(path));
+	ft_strcpy(nw_path, home);
+	ft_strcat(nw_path, "/");
+	tmp = ft_substr(path, 2, ft_strlen(path));
+	ft_strcat(nw_path, tmp);
+	accessible = access(nw_path, F_OK);
+	if (accessible != -1)
+		chdir(nw_path);
+	free(nw_path);
+	if (tmp)
+		free(tmp);
+	return (accessible);
+}
+
 int	ft_cd(char *cwd, char *path, char **cmd)
 {
+	char	*home;
+
+	home = getenv("HOME");
 	if (!ft_strncmp(cmd[0], "cd", 3))
 	{
 		if (!cmd[1] || (cmd[1][0] == '~' && ft_strlen(cmd[1]) == 1))
-			chdir(getenv("HOME"));
+			chdir(home);
+		else if (cmd[1][0] == '~' && ft_strlen(cmd[1]) > 1)
+		{
+			if (check_if_raccessible(cwd, cmd[1]) == -1)
+				perror("cd");
+		}
 		else
 			if (check_if_accessible(cwd, cmd[1]) == -1)
-				printf("cd: no such file or directory: %s\n", cmd[1]);
+				perror("cd");
 		return (1);
 	}
 	return (0);
