21695ebd114618855e5e90668c7b04d63ac2aa05
0xySan <ethtaquet@gmail.com>
2024-12-06 22:44:30 +0100

    corrected sum error + get cmd path

--- test.c
+++ test.c
@@ -6,13 +6,104 @@
 /*   By: etaquet <etaquet@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 16:18:53 by etaquet           #+#    #+#             */
-/*   Updated: 2024/12/06 19:00:09 by etaquet          ###   ########.fr       */
+/*   Updated: 2024/12/06 22:44:09 by etaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /// to use it : gcc -o test test.c -lreadline -lhistory libft/libft.a
 
 #include "test.h"
+
+void	print_graffiti()
+{
+	printf("\x1B[35m    __                    __         __   \x1B[33m___  __");
+	printf("______ \x1B[36m __  __   __\n\x1B[35m   / /   ____  ____ _____/ /");
+	printf("__  ____/ /  \x1B[33m|__ \\<  / ___/\x1B[36m / / / /  / /\n");
+	printf("\x1B[35m  / /   / __ \\/ __ `/ __  / _ \\/ __  /   \x1B[33m__/ //");
+	printf(" /\\__ \\\x1B[36m / /_/ /  / / \n\x1B[35m / /___/ /_/ / /_/ / /_/");
+	printf(" /  __/ /_/ /   \x1B[33m/ __// /___/ /\x1B[36m/ __  /  /_/  \n");
+	printf("\x1B[35m/_____/\\____/\\__,_/\\__,_/\\___/\\__,_/   \x1B[33m");
+	printf("/____/_//____/\x1B[36m/_/ /_/  (_)   \n\n");
+}
+
+int	check_if_only_space(char *str)
+{
+	int	i;
+	int	v;
+
+	i = 0;
+	v = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ')
+			i++;
+		else
+		{
+			v++;
+			i++;
+		}
+	}
+	return (v);
+}
+
+void	free_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	if (!args)
+		return ;
+	while (args[i])
+	{
+		if (args[i])
+			free(args[i]);
+		i++;
+	}
+	if (args)
+		free(args);
+}
+
+char	**getpath()
+{
+	char	**envpath;
+	int		i;
+
+	i = -1;
+	envpath = NULL;
+	envpath = ft_split(getenv("PATH"), ':');
+	if (envpath)
+		return (envpath);
+	return (NULL);
+}
+
+char	*get_cmd_path(char *arg)
+{
+	char	*cpath;
+	char	**mp;
+	int		i;
+
+	i = 0;
+	if (access(arg, X_OK) != -1)
+		return (arg);
+	mp = getpath();
+	if (!mp)
+		return (NULL);
+	while (mp[i])
+	{
+		cpath = malloc(sizeof(char) * (ft_strlen(mp[i]) + ft_strlen(arg) + 2));
+		if (!cpath)
+			return (NULL);
+		ft_strcpy(cpath, mp[i]);
+		ft_strcat(cpath, "/");
+		ft_strcat(cpath, arg);
+		if (access(cpath, X_OK) != -1)
+			return (free_args(mp), cpath);
+		free(cpath);
+		i++;
+	}
+	free_args(mp);
+	return (NULL);
+}
 
 void	sigint_handler(int sig)
 {
@@ -114,25 +205,47 @@ int	ft_cd(char *cwd, char *path, char **cmd)
 
 void	child_process(char **cmd, char **envp, t_pidstruct	*pid)
 {
+	char	*actual_cmd;
+
+	actual_cmd = get_cmd_path(cmd[0]);
 	pid->pid[0] = fork();
 	if (pid->pid[0] == -1)
 		perror("Error: Fork failed.\n");
 	if (pid->pid[0] == 0)
 	{
-		if (access(ft_strjoin("/bin/", cmd[0]), X_OK) != -1)
-			execve(ft_strjoin("/bin/", cmd[0]), cmd, envp);
+		if (access(actual_cmd, X_OK) != -1)
+			execve(actual_cmd, cmd, envp);
 		else
 			perror("21sh");
 	}
+}
+
+void    execute_input(char *cwd, char **env, t_pidstruct *pid, char *input)
+{
+	char	**cmd;
+	char    *cmd_path;
+
+	cmd = ft_split(input, ' ');
+	if (ft_cd(cwd, input, cmd))
+	    return ;
+	cmd_path = get_cmd_path(cmd[0]);
+	if (access(cmd_path, X_OK) != -1)
+	{
+		child_process(cmd, env, pid);
+		waitpid(pid->pid[0], NULL, 0);
+		free(cmd_path);
+	}
+	else
+	    perror("21sh");
+	free_args(cmd);
 }
 
 int	read_lines(char *cwd, char **env, t_pidstruct *pid)
 {
 	char	*input;
 	char	*r_path;
-	char	**cmd;
 
-	pid->pid = malloc(sizeof(pid_t) * 20);
+	pid->pid = malloc(sizeof(pid_t) * 1);
 	signal(SIGQUIT, sigquit_handler);
 	r_path = get_relative_path(cwd);
 	printf("\e[1m\x1B[31m");
@@ -143,15 +256,10 @@ int	read_lines(char *cwd, char **env, t_pidstruct *pid)
 	if (input)
 	{
 		add_history(input);
-		cmd = ft_split(input, ' ');
-		if (!ft_cd(cwd, input, cmd))
-		{
-			child_process(cmd, env, pid);
-			waitpid(pid->pid[0], NULL, 0);
-		}
+		execute_input(cwd, env, pid, input);
+		free(input);
 	}
 	free(pid->pid);
-	free(input);
 	free(r_path);
 	return (0);
 }
@@ -163,6 +271,7 @@ int	main(int argc, char **argv, char **env)
 	t_pidstruct	pid;
 
 	signal(SIGINT, sigint_handler);
+	print_graffiti();
 	while (1)
 	{
 		if (getcwd(cwd, sizeof(cwd)) == NULL)
