412dc102e7b2fb922fab541687ea0d1465474d2a
hugo <hugodelacour.pro@gmail.com>
2025-02-10 23:29:23 +0100

    fix: clarified ft_echo command, removed "n" variable

--- ft_cmds/ft_echo.c
+++ ft_cmds/ft_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etaquet <etaquet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hdelacou <hdelacou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 16:56:24 by etaquet           #+#    #+#             */
-/*   Updated: 2025/02/09 22:57:13 by etaquet          ###   ########.fr       */
+/*   Updated: 2025/02/10 23:25:31 by hdelacou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,14 @@ int	check_if_only_char(char *str, int start, char car)
 void	ft_echo(char **cmd)
 {
 	int	i;
-	int	n;
 
 	i = 1;
-	n = (cmd[i] && cmd[i][0] == '-' && check_if_only_char(cmd[i], 1, 'n'));
-	if (n)
+	if (cmd[i] && cmd[i][0] == '-' && check_if_only_char(cmd[i], 1, 'n'))
 		i++;
 	while (cmd[i] && cmd[i + 1])
 		printf("%s ", cmd[i++]);
 	if (cmd[i])
 		printf("%s", cmd[i]);
-	if (!n)
+	if (cmd[i] && cmd[i][0] != '-' && !check_if_only_char(cmd[i], 0, 'n'))
 		printf("\n");
 }
