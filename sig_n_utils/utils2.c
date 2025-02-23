49fb4515e3dc3a015ac62f33e522f4d1219c7e3b
0xySan <ethtaquet@gmail.com>
2025-02-23 19:35:01 +0100

    removed unused werror

--- sig_n_utils/utils2.c
+++ sig_n_utils/utils2.c
@@ -6,7 +6,7 @@
 /*   By: etaquet <etaquet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 16:46:49 by etaquet           #+#    #+#             */
-/*   Updated: 2025/02/23 06:26:25 by etaquet          ###   ########.fr       */
+/*   Updated: 2025/02/23 19:34:30 by etaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ char	*ft_strjoin_join(const char *s1, const char *s2, const char *s3)
 void	edit_lvl(char **env)
 {
 	char	*lvl;
-	int		i;
 	int		lvl_int;
 	char	*itoa_value;
 
@@ -44,7 +43,6 @@ void	edit_lvl(char **env)
 		return ;
 	lvl_int = ft_atoi(lvl);
 	lvl_int++;
-	i = -1;
 	itoa_value = ft_itoa(lvl_int);
 	ft_change_env(env, "SHLVL", itoa_value);
 	free(itoa_value);
